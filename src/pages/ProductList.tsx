1e55c3f84ba022f5146f41fa7f9611f38b06dfb0
ElieHousseini <eliashousseini@gmail.com>
2023-04-10 13:39:25 +0400

    remove context api

--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -1,24 +1,15 @@
-import { useState } from "react"
 import Footer from "../components/Footer/Footer"
 import Header from "../components/Header/ProductList/Header"
 import Products from "../components/Products/Products"
 import Layout from "../Layout/Layout"
-import ItemsToDeleteContext from "../context/itemsToDeleteContext"
-import {ProductType} from '../types'
-
-type ItemsToDeleteType = string[]
 
 const ProductList = () : JSX.Element => {
-    const [itemsToDelete, setItemsToDelete] = useState<ItemsToDeleteType>([]);
-    const [products, setProducts] = useState<ProductType[]>([]);
-
+    
     return (
         <Layout>
-            <ItemsToDeleteContext.Provider value = {{itemsToDelete, setItemsToDelete, products, setProducts}}>
                 <Header />
                 <Products />
                 <Footer text='Lib System' />
-            </ItemsToDeleteContext.Provider>
         </Layout>
     )
 }
